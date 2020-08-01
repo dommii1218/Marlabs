60bc34a0f37757bd3661b7841a29aeda22ff50ca
dommii1218 <46981604+dommii1218@users.noreply.github.com>
2020-08-01 01:55:59 -0400

    Update TS.ts

--- TS.ts
+++ TS.ts
@@ -85,9 +85,7 @@ console.log(add8(10, 20)); //returns 50
 
 // 9. Function Overloading
 function add(a:string, b:string):string;
-
 function add(a:number, b:number): number;
-
 function add(a: any, b:any): any {
     return a + b;
 }
